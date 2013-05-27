965ac80b7f508c8f6d1b3bdfff7104e65baeb250
aleksey93 <golov_a29@mail.ru>
2013-05-27 19:28:48 +0400

    Create Clear.php

---
+++ Clear.php
@@ -0,0 +1,48 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+define("DBName","bd");
+define("HostName","localhost");
+define("UserName","root");
+define("Password","");
+if(!mysql_connect(HostName,UserName,Password))
+{ echo "Error connecting to database".DBName."!<br>";
+echo mysql_error();
+exit;
+}
+
+mysql_select_db(DBName);
+ $r=mysql_query("drop database if exists ".DBName);
+
+ $r=mysql_query("create database ".DBName);
+mysql_select_db(DBName);
+
+ $r=mysql_query('SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO"');
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
+ $r=mysql_query('SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO"');
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
+ $r=mysql_query('SET time_zone = "+00:00"');
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
+ $r=mysql_query("CREATE TABLE IF NOT EXISTS `testbd` (  `name1` text NOT NULL,  `name2` text NOT NULL,  `name3` int NOT NULL,  `service` int NOT NULL,  `frequency` int NOT NULL,  `kitchen` int NOT NULL, better_work int NOT NULL, `question` int NOT NULL,  `data` int NOT NULL,  `telephone` int NOT NULL,  `password` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
+ $r=mysql_query("INSERT INTO `testbd` (`name1`  `name2`   `name3` ,  `service`,  `frequency` ,  `kitchen` , better_work , `question` ,  `data`,  `telephone`,  `password`) VALUES ('', '', '100', '100', '100', '1000', '100', '100', '? ???????', '1', ''),('', '', '100', '100', '100', '1000', '100', '100', '0', '1', '');");
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+ 
